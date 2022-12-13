290ab987aef0a95b9085d03da5eaadeb3a48b381
bonnyr <bonnyr@gmail.com>
2022-12-13 22:50:38 +1100

    fix: using wrong comment marker...

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # If Makefile exists, use it and assume it builds in a similar fashion to the clang command below.
-// Otherwise copy the template one over and use it
+# Otherwise copy the template one over and use it
 if ! [-f Makefile ]; then
     cp .action/Makefile .
 fi
