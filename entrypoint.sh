61e5726f8638689dcf3730cd71aa7b451030eaa1
bonnyr <bonnyr@gmail.com>
2022-12-13 15:07:02 +1100

    removed commented code and changed to use default Makefile if one is not present

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 # If Makefile exists, use it and assume it builds in a similar fashion to the clang command below.
-// Useful for more complex repo structures
+// Otherwise copy the template one over and use it
 if ! [-f Makefile ]; then
     cp .action/Makefile .
 fi
 make all
-
-# else    
-#     mkdir -p dist
-#     clang --target=wasm32-unknown-wasi --sysroot /opt/wasi-libc -nostartfiles -Wl,--import-memory -Wl,--export-table -Wl,--no-entry -Werror -o dist/chip.wasm $SOURCES
-# fi
